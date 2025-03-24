f7876550cf04fe87ff5b063fe5daa2ba82010168
Mihir Jain <Mihir.Jain@veritas.com>
2025-03-24 23:24:25 +0530

    Added dep on dog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jainmihir26/go-puppy
 
 go 1.24.0
+
+require github.com/jainmihir26/go-dog v0.0.0-20250324175159-d98ea2aeece7 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	dog "github.com/jainmihir26/go-dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Bark! Bark! Bark!"
+}
+
+func BarkFromDog() string {
+	return dog.BarkInDog()
 }
